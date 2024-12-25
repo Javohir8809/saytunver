4fb13bea8d4243420dbab8e2e74a1e90741b00e2
Diyorbek <bekmurodovdiyorbek288@gmail.com>
2024-12-25 10:35:42 +0500

    fdfsfsdf

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,13 @@ import { useTranslation } from "react-i18next"; // i18next ni chaqirish
 import Select from "react-select"; // react-select ni import qilish
 import saytlogo from "../assets/photo_2021-11-03_09-02-49.jpg";
 import bayroq from "../assets/bayroq.jpg";
-import Girb from "../assets/Gerb.png";
+import Girb from "../assets/img/gerb_uzbekistana.png";
 import { FaMusic } from "react-icons/fa";
 import "../assets/style/font.css";
 import { MdArrowDropDown } from "react-icons/md";
-import { Link } from "react-router";
+import { IoMenu } from "react-icons/io5";
+
+import '../assets/style/font.css'
 const Header = () => {
   const { t, i18n } = useTranslation(); // i18next dan foydalanish
   const [fontSizeClass, setFontSizeClass] = useState("text-base");
@@ -39,13 +41,6 @@ const Header = () => {
     { value: "en", label: "English" },
   ];
 
-  const handleNavigation = (event) => {
-    const url = event.target.value;
-    if (url) {
-      window.location.href = url;
-    }
-  };
-
   return (
     <div className="fixed  top-0 left-0 w-full bg-beige/90 backdrop-blur-md border-b border-gray-300 z-50 h-16">
       <div className="flex items-center justify-between  py-2 px-[20px] max-w-[1210px] mx-auto">
@@ -55,13 +50,13 @@ const Header = () => {
           <img
             src={bayroq}
             alt="UZ"
-            className="h-6 mr-2 cursor-pointer hover:scale-105 hover:bg-gray-200 p-1 rounded"
+            className="h-auto w-10 mr-2 cursor-pointer hover:scale-105 hover:bg-gray-200 p-1 rounded"
           />
           <span className="hover:text-blue-500 cursor-pointer">|</span>
           <img
             src={Girb}
-            alt="UZ"
-            className="h-5 mr-2 cursor-pointer hover:scale-105 hover:bg-gray-200 p-1 rounded"
+            alt="UZ.Gerbi"
+            className="h-auto w-10 mr-2 cursor-pointer hover:scale-105 hover:bg-gray-200 p-1 rounded"
           />
           <span className="hover:text-blue-500 cursor-pointer">|</span>
           <div>
@@ -94,26 +89,30 @@ const Header = () => {
       <div className="bg-[#1B6B50] text-white border-b-2 border-t-2  bg-beige/90 backdrop-blur-md  border-gray-300  ">
         <div className="container max-w-[1210px] px-[20px] mx-auto">
           <header className={`flex items-center  ${fontSizeClass}`}>
-            <nav className="flex items-center py-2 w-full justify-between">
-              <ul className="flex items-center gap-2 ">
+            <nav className="navbar flex items-center py-2 w-full justify-between">
+              <ul className="ul_sayt flex items-center gap-2">
                 <img
                   src={saytlogo}
                   alt="Logo"
                   className="h-15 w-[50px] h-auto"
                 />
-                <div className="max-w-[290px]">
+                <div className="max-w-[290px] samdu">
                   <span className="text-[14px]">{t("university")}</span>
                 </div>
               </ul>
 
-              <ul className="flex items-center justify-between  max-w-[800px] gap-3">
+              <ul className="li_ul flex items-center justify-between  max-w-[800px] gap-3">
                 {/* Dropdown menu */}
                 <li
-                  className="relative"
+                  className="relative flex"
                   onMouseEnter={() => setIsDropdownOpen(true)} // Hover qilganda menyuni ochish
                   onMouseLeave={() => setIsDropdownOpen(false)} // Hoverdan chiqganda menyuni yopish
                 >
-                  <button className=" text-white items-center font-sans relative hover:text-white flex  hover:bg-blue-500 ">
+                  <button className="flex items-center gap-1 hover:text-yellow-400">
+                    <MdArrowDropDown />
+                  </button>
+
+                  <button className=" text-white items-center font-sans relative hover:text-white flex  ">
                     {t("filial")}
                   </button>
                   {isDropdownOpen && (
@@ -149,11 +148,15 @@ const Header = () => {
                 </li>
 
                 <li
-                  className="relative"
+                  className="relative flex"
                   onMouseEnter={() => setIsDropdownOpen2(true)} // Hover qilganda menyuni ochish
                   onMouseLeave={() => setIsDropdownOpen2(false)} // Hoverdan chiqganda menyuni yopish
                 >
-                  <button className=" text-white items-center font-sans relative hover:text-white flex  hover:bg-blue-500 ">
+                  <button className="flex items-center gap-1 hover:text-yellow-400">
+                    <MdArrowDropDown />
+                  </button>
+
+                  <button className=" text-white items-center font-sans relative hover:text-white flex  ">
                     {t("faoliyat")}
                   </button>
                   {isDropdownOpen2 && (
@@ -189,11 +192,15 @@ const Header = () => {
                 </li>
 
                 <li
-                  className="relative"
+                  className="relative flex"
                   onMouseEnter={() => setIsDropdownOpen3(true)} // Hover qilganda menyuni ochish
                   onMouseLeave={() => setIsDropdownOpen3(false)} // Hoverdan chiqganda menyuni yopish
                 >
-                  <button className=" text-white items-center font-sans relative hover:text-white flex  hover:bg-blue-500 ">
+                  <button className="flex items-center gap-1 hover:text-yellow-400">
+                    <MdArrowDropDown />
+                  </button>
+
+                  <button className=" text-white items-center font-sans relative hover:text-white flex ">
                     {t("info")}
                   </button>
                   {isDropdownOpen3 && (
@@ -229,11 +236,15 @@ const Header = () => {
                 </li>
 
                 <li
-                  className="relative"
+                  className="relative flex"
                   onMouseEnter={() => setIsDropdownOpen4(true)} // Hover qilganda menyuni ochish
                   onMouseLeave={() => setIsDropdownOpen4(false)} // Hoverdan chiqganda menyuni yopish
                 >
-                  <button className=" text-white items-center font-sans relative hover:text-white flex  hover:bg-blue-500 ">
+                  <button className="flex items-center gap-1 hover:text-yellow-400">
+                    <MdArrowDropDown />
+                  </button>
+
+                  <button className=" text-white items-center font-sans relative hover:text-white flex ">
                     {t("hujjatlar")}
                   </button>
                   {isDropdownOpen4 && (
@@ -269,11 +280,14 @@ const Header = () => {
                 </li>
 
                 <li
-                  className="relative"
+                  className="relative flex"
                   onMouseEnter={() => setIsDropdownOpen5(true)} // Hover qilganda menyuni ochish
                   onMouseLeave={() => setIsDropdownOpen5(false)} // Hoverdan chiqganda menyuni yopish
                 >
-                  <button className=" text-white items-center font-sans relative hover:text-white flex  hover:bg-blue-500 ">
+                  <button className="flex items-center gap-1 hover:text-yellow-400">
+                    <MdArrowDropDown />
+                  </button>
+                  <button className=" text-white items-center font-sans relative hover:text-white flex   ">
                     {t("talabalar")}{" "}
                   </button>
                   {isDropdownOpen5 && (
@@ -309,11 +323,13 @@ const Header = () => {
                 </li>
 
                 <li
-                  className="relative"
-                  onMouseEnter={() => setIsDropdownOpen6(true)} // Hover qilganda menyuni ochish
-                  onMouseLeave={() => setIsDropdownOpen6(false)} // Hoverdan chiqganda menyuni yopish
+                  className="relative flex"
+                  // Hoverdan chiqganda menyuni yopish
                 >
-                  <button className=" text-white items-center font-sans relative hover:text-white flex  hover:bg-blue-500 ">
+                  <button className="flex items-center gap-1 hover:text-yellow-400">
+                    <MdArrowDropDown />
+                  </button>
+                  <button className=" text-white items-center font-sans relative hover:text-white flex  ">
                     {t("aloqa")}{" "}
                   </button>
                   {isDropdownOpen6 && (
@@ -344,10 +360,18 @@ const Header = () => {
                       <li className="hover:bg-blue-700 px-4 py-2 cursor-pointer text-black">
                         Ochiq ma'lumotlar
                       </li>
+                    
                     </ul>
                   )}
                 </li>
+               
               </ul>
+              
+             <ul>
+             <button className="menubtn" >
+                     <IoMenu className="w-10 h-10 "/>
+                     </button>
+             </ul>
             </nav>
           </header>
         </div>
